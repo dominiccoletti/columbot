70ebd34087e646c729d1d85ed0679416d6d855b1
Dominic Coletti <dcoletti.dcair@gmail.com>
2019-04-07 22:05:08 -0400

    Added lion #2 to !sad

--- modules/sad.py
+++ modules/sad.py
@@ -5,7 +5,8 @@ import random
 class Sad(Module):
     DESCRIPTION = "Instantly dispel all mental health problems"
     PICTURES = [
-        "https://i.groupme.com/600x415.jpeg.9b3642af63514f74b190b08d14c4cffc.large"  # cat
+        "https://i.groupme.com/1600x760.jpeg.20c03d30afa64f52a65c6f896f5c32dd.large",
+        "https://i.groupme.com/600x415.jpeg.9b3642af63514f74b190b08d14c4cffc.large",  # cat
     ]
 
     def response(self, query, message):
